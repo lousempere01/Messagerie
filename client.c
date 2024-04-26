3ebee052a9185e26ffe8081bf3948c0e1f156749
Lou Sempere <lou.sempere01@etu.umontpellier.fr>
2024-04-26 13:20:07 +0200

    v1

--- client.c
+++ client.c
@@ -3,68 +3,150 @@
 #include <arpa/inet.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <pthread.h>
+#include <signal.h>
+
+#define max_length 50
+char msg[max_length];
+char input[max_length]; 
+
+void *readMessage(void *arg) {
+    int dS = *(int *)arg;
+    int nb_recv;
+
+    while(1) {
+
+        // Read message
+        nb_recv = recv(dS, msg, max_length, 0);
+        if (nb_recv == -1) {
+            perror("Erreur lors de la reception du message");
+            close(dS);
+            exit(EXIT_FAILURE);
+        } else if (nb_recv == 0) {
+            // Connection closed by client or server
+            break;
+        }
+
+        // Check if the message is "fin"
+        // If it is, close the socket and exit
+        if (strcmp(msg, "fin") == 0) {
+            printf("Un client a quitté la discussion\n");
+        } else {
+            printf("Message reçu: %s\n", msg);
+        }
+    }
+
+    pthread_exit(0);
+}
+
+void *writeMessage(void *arg) {
+    int dS = *(int *)arg;
+    int nb_send;
+
+    while(1) {
+        fgets(input, max_length, stdin); 
+        char *pos = strchr(input, '\n');
+        *pos = '\0';
+
+        printf("Message envoyé: %s\n", input);
+        
+        // Send message
+        nb_send = send(dS, input, max_length, 0);
+        if (nb_send == -1) {
+            perror("Erreur lors de l'envoi du message");
+            close(dS);
+            exit(EXIT_FAILURE);
+        } else if (nb_send == 0) {
+            // Connection closed by remote host
+            printf("Le serveur a fermé la connexion\n");
+            close(dS);
+            break;
+        }
+
+        // Check if the message is "fin"
+        // If it is, close the socket and exit
+        if (strcmp(input, "fin") == 0) {
+            printf("Vous avez mis fin à la discussion\n");
+            close(dS);
+            break;
+        }
+    }
+
+    pthread_exit(0);
+}
+
+void handle_sigint(int sig) {
+    printf("Pour quitter, veuillez saisir le mot 'fin'.\n");
+}
 
 int main(int argc, char *argv[]) {
     if (argc != 3) {
-        fprintf(stderr, "Usage: %s <IP> <Port>\n", argv[0]);
+        printf("Error: You must provide exactly 2 arguments.\n\
+                Usage: %s <server_ip> <server_port>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
-    printf("Début programme\n");
+    printf("Début programme client\n");
+    printf("Bienvenue sur la messagerie instantanée !\n");
 
-    // Création du socket client
     int dS = socket(PF_INET, SOCK_STREAM, 0);
     if (dS == -1) {
-        perror("Erreur lors de la création du socket");
+        perror("Erreur création socket");
         exit(EXIT_FAILURE);
     }
 
-    // Configuration de l'adresse et du port du serveur
+    printf("Socket Créé\n");
+
     struct sockaddr_in aS;
     aS.sin_family = AF_INET;
-    inet_pton(AF_INET, argv[1], &(aS.sin_addr));
-    aS.sin_port = htons(atoi(argv[2]));
-    socklen_t lgA = sizeof(struct sockaddr_in);
-
-    // Connexion au serveur
-    if (connect(dS, (struct sockaddr *) &aS, lgA) == -1) {
-        perror("Erreur lors de la connexion au serveur");
+    int result = inet_pton(AF_INET, argv[1], &(aS.sin_addr));
+    if (result == 0) {
+        fprintf(stderr, "Adresse invalide\n");
+        exit(EXIT_FAILURE);
+    } else if (result == -1) {
+        perror("inet_pton");
         exit(EXIT_FAILURE);
     }
 
-    printf("Connecté au serveur\n");
-
-    char message[1024];
-    while (1) {
-        // Demande de saisie d'un message à envoyer au serveur
-        printf("Entrez votre message : ");
-        fgets(message, sizeof(message), stdin);
-
-        // Suppression du caractère de nouvelle ligne de la saisie
-        message[strcspn(message, "\n")] = 0;
-
-        // Envoi du message au serveur
-        send(dS, message, strlen(message), 0);
-
-        // Vérification si le message est "fin", si oui, terminer la connexion
-        if (strcmp(message, "fin") == 0) {
-            printf("Envoi du message 'fin', terminant la connexion.\n");
-            break;
-        }
-
-        // Réception de la réponse du serveur
-        recv(dS, message, sizeof(message), 0);
-        printf("Réponse reçue du serveur : %s\n", message);
-
-        // Vérification si le message reçu est "fin", si oui, terminer la connexion
-        if (strcmp(message, "fin") == 0) {
-            printf("Le serveur a envoyé 'fin', terminant la connexion.\n");
-            break;
-        }
+    aS.sin_port = htons(atoi(argv[2])) ;
+    socklen_t lgA = sizeof(struct sockaddr_in) ;
+    if (connect(dS, (struct sockaddr *) &aS, lgA) == -1) {
+        perror("Erreur connect client");
+        exit(EXIT_FAILURE);
     }
 
-    // Fermeture de la connexion avec le serveur
-    shutdown(dS, 2);
-    printf("Connexion fermée\n");
-    return 0;
+    printf("Socket Connecté\n\n");
+
+    signal(SIGINT, handle_sigint);
+
+    pthread_t readThread;
+    pthread_t writeThread;
+
+    if (pthread_create(&readThread, NULL, readMessage, &dS) != 0) {
+        perror("Erreur création thread de lecture");
+        close(dS);
+        exit(EXIT_FAILURE);
+    }
+
+    if (pthread_create(&writeThread, NULL, writeMessage, &dS) != 0) {
+        perror("Erreur création thread d'écriture");
+        close(dS);
+        exit(EXIT_FAILURE);
+    }
+
+    if (pthread_join(readThread, NULL) != 0) {
+        perror("Erreur fermeture thread de lecture");
+        close(dS);
+        exit(EXIT_FAILURE);
+    }
+
+    if (pthread_join(writeThread, NULL) != 0) {
+        perror("Erreur fermeture thread d'écriture");
+        close(dS);
+        exit(EXIT_FAILURE);
+    }
+
+    return EXIT_SUCCESS;
 }
--- serveur.c
+++ serveur.c
@@ -3,79 +3,147 @@
 #include <arpa/inet.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <pthread.h>
+
+#define MAX_CLIENT 2
+#define NB_THREADS 2
+#define BUFFER_SIZE 50
+
+int tab_client[MAX_CLIENT];
+
+// Fonction exécutée par chaque thread pour relayer les messages
+void * broadcast(void * dS_client) {
+    int dSC = *(int *)dS_client;
+    char buffer[BUFFER_SIZE];
+    int nb_recv;
+    int nb_send;
+    int i;
+
+    // Boucle infinie pour écouter les messages du client
+    while (1) {
+        // Recevoir le message du client
+        nb_recv = recv(dSC, buffer, BUFFER_SIZE, 0);
+        if (nb_recv == -1) {
+            perror("Erreur lors de la reception");
+            exit(EXIT_FAILURE);
+        }
+        if (nb_recv == 0) {
+            printf("Client déconnecté\n");
+            break;
+        }
+
+        printf("Message recu: %s du client dSC: %d \n", buffer, dSC);
+
+        // Si le client envoie "fin", arrêter la discussion pour ce client
+        if (strcmp(buffer, "fin") == 0) {
+            printf("Fin de la discussion pour client dSC: %d\n", dSC);
+            break;
+        }
+
+        // Envoyer le message aux autres clients
+        i = 0;
+        while( i < MAX_CLIENT && tab_client[i] != 0){
+            if (tab_client[i] != dSC) {
+                nb_send = send(tab_client[i], buffer, BUFFER_SIZE, 0);
+                if (nb_send == -1) {
+                    perror("Erreur lors de l'envoi");
+                    exit(EXIT_FAILURE);
+                }
+                if (nb_send == 0) {
+                    printf("Le client dSC: %d s'est deconnecte\n", tab_client[i]);
+                }
+            }
+            i = i + 1;
+        }
+    }
+
+    // Fermer le socket du client
+    if (close(dSC) == -1) {
+        perror("Erreur lors de la fermeture du descripteur de fichier");
+        exit(EXIT_FAILURE);
+    }
+
+    // Mettre 0 dans le tableau pour indiquer que le client s'est déconnecté
+    *(int *)dS_client = 0;
+
+    pthread_exit(0);
+}
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
-        fprintf(stderr, "Usage: %s <Port>\n", argv[0]);
+        printf("Error: You must provide exactly 1 argument.\nUsage: ./serv <port>\n");
         exit(EXIT_FAILURE);
-    }
+    }  
 
-    printf("Début du programme\n");
+    printf("Debut du Serveur.\n");
 
-    // Création du socket serveur
     int dS = socket(PF_INET, SOCK_STREAM, 0);
-    if (dS == -1) {
+    if(dS == -1) {
         perror("Erreur lors de la création du socket");
         exit(EXIT_FAILURE);
     }
-    printf("Socket Créé\n");
 
-    // Configuration de l'adresse et du port du serveur
     struct sockaddr_in ad;
     ad.sin_family = AF_INET;
-    ad.sin_addr.s_addr = INADDR_ANY;
-    ad.sin_port = htons(atoi(argv[1]));
-
-    // Association du socket avec l'adresse et le port
-    if (bind(dS, (struct sockaddr*)&ad, sizeof(ad)) == -1) {
+    ad.sin_addr.s_addr = INADDR_ANY ;
+    ad.sin_port = htons(atoi(argv[1])) ;
+    if(bind(dS, (struct sockaddr*)&ad, sizeof(ad)) == -1) {
         perror("Erreur lors du nommage du socket");
         exit(EXIT_FAILURE);
     }
-    printf("Socket Nommé\n");
 
-    // Mise en écoute du socket pour les connexions entrantes
-    if (listen(dS, 2) == -1) {
-        perror("Erreur en mettant le socket en mode écoute");
+    if(listen(dS, 10) == -1) {
+        perror("Erreur lors du passage en mode écoute");
         exit(EXIT_FAILURE);
     }
-    printf("Mode écoute\n");
 
-    // Attente de la connexion de deux clients
-    struct sockaddr_in aC;
-    socklen_t lg = sizeof(struct sockaddr_in);
-    int dSC1 = accept(dS, (struct sockaddr*) &aC, &lg);
-    printf("Client 1 Connecté\n");
+    memset(tab_client, 0, sizeof(tab_client));
+    struct sockaddr_in tab_adr[MAX_CLIENT];
+    socklen_t tab_lg[MAX_CLIENT];
 
-    int dSC2 = accept(dS, (struct sockaddr*) &aC, &lg);
-    printf("Client 2 Connecté\n");
+    printf("En attente de connexion des clients\n");
 
-    char message[1024];
-    while (1) {
-        // Client 1 écrit puis client 2 lit
-        recv(dSC1, message, sizeof(message), 0);
-        printf("Client 1 a envoyé : %s\n", message);
-        send(dSC2, message, strlen(message), 0);
-
-        if (strcmp(message, "fin") == 0) {
-            printf("Client 1 a envoyé 'fin', terminant la connexion.\n");
-            break;
+    // Accepter les connexions des clients
+    for (int i = 0; i < MAX_CLIENT; i++){
+        tab_lg[i] = sizeof(struct sockaddr_in);
+        tab_client[i] = accept(dS, (struct sockaddr*) &tab_adr[i],&tab_lg[i]) ;
+        if(tab_client[i] == -1) {
+            perror("Erreur lors de la connexion avec le client");
+            exit(EXIT_FAILURE);
         }
+        printf("Client %d connecté\n", i+1);
+    }
+  
+    pthread_t tid;
+    pthread_t Threads_id [NB_THREADS] ;
 
-        // Client 2 écrit puis client 1 lit
-        recv(dSC2, message, sizeof(message), 0);
-        printf("Client 2 a envoyé : %s\n", message);
-        send(dSC1, message, strlen(message), 0);
-
-        if (strcmp(message, "fin") == 0) {
-            printf("Client 2 a envoyé 'fin', terminant la connexion.\n");
-            break;
+    // Créer des threads pour gérer la communication avec les clients
+    for (int i = 0; i < NB_THREADS; i++){
+        if (pthread_create(&tid, NULL, broadcast, (void *) &tab_client[i]) != 0) {
+            perror("Erreur lors de la création du thread");
+            exit(EXIT_FAILURE);
+        }
+        else{
+            Threads_id[i] = tid;
         }
     }
 
-    // Fermeture des sockets
-    shutdown(dSC1, 2);
-    shutdown(dSC2, 2);
-    shutdown(dS, 2);
-    printf("Fin du programme\n");
-    return 0;
+    // Attendre que les threads se terminent
+    for (int i = 0; i < NB_THREADS; i++){
+        if (pthread_join(Threads_id[i], NULL) != 0) {
+            perror("Erreur lors de l'attente de la fin du thread");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    printf("Both clients disconnected\n");
+
+    if (close(dS)==-1){
+        perror("Erreur lors de la fermeture du socket de acceptation");
+        exit(EXIT_FAILURE);
+    }
+
+    return 1;  
 }
