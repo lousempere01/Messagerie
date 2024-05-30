f82785a42908bc879fd47fd58a3ca2172f0a76b7
Zolan <zolannp2602@gmail.com>
2024-05-30 17:02:39 +0200

    modif

--- clientv2.1.c
+++ clientv2.1.c
@@ -7,16 +7,61 @@
 #include <errno.h>
 #include <pthread.h>
 #include <signal.h>
+#include <dirent.h>
 
 #define MAX_LEN 1000
 char msg[MAX_LEN];
 char input[MAX_LEN];
 char pseudo[MAX_LEN];
 
-pthread_mutex_t mutex =
-    PTHREAD_MUTEX_INITIALIZER; // Mutex pour la deconnexion
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+int is_connected = 1;
 
-int is_connected = 1; // Drapeau pour indiquer si le client est connecté
+void listFiles() {
+    DIR *d;
+    struct dirent *dir;
+    d = opendir(".");
+    if (d) {
+        while ((dir = readdir(d)) != NULL) {
+            printf("%s\n", dir->d_name);
+        }
+        closedir(d);
+    }
+}
+
+void sendFileInfo(int dS, char *filename) {
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) {
+        perror("Erreur lors de l'ouverture du fichier");
+        return;
+    }
+
+    fseek(file, 0, SEEK_END);
+    long filesize = ftell(file);
+    rewind(file);
+
+    char buffer[MAX_LEN];
+    sprintf(buffer, "%s %ld", filename, filesize);
+    send(dS, buffer, strlen(buffer), 0);
+
+    fclose(file);
+}
+
+void sendFile(int dS, char *filename) {
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) {
+        perror("Erreur lors de l'ouverture du fichier");
+        return;
+    }
+
+    char buffer[MAX_LEN];
+    size_t bytes;
+    while ((bytes = fread(buffer, 1, MAX_LEN, file)) > 0) {
+        send(dS, buffer, bytes, 0);
+    }
+
+    fclose(file);
+}
 
 void displayManual() {
     FILE *manuel = fopen("manuel.txt", "r");
@@ -33,79 +78,68 @@ void displayManual() {
     fclose(manuel);
 }
 
-void *readMessage(void *arg)
-{
+void *readMessage(void *arg) {
     int dS = *(int *)arg;
     int nb_recv;
-
-    while (1)
-    {
-        
-
-        // Read message
+    while (1) {
         nb_recv = recv(dS, msg, MAX_LEN, 0);
         pthread_mutex_lock(&mutex);
-        if (!is_connected)
-        {
+        if (!is_connected) {
             break;
-        }else if (strcmp(msg, "/fin") == 0) {
+        } else if (strcmp(msg, "/fin") == 0) {
             printf("Un client a quitté la discussion\n");
-        }
-        else if (strncmp(msg, "/mp", 3) == 0)
-        {
+        } else if (strncmp(msg, "/mp", 3) == 0) {
             char destinataire[MAX_LEN];
             char contenu[MAX_LEN];
             sscanf(msg, "/mp %s %[^\n]", destinataire, contenu);
             printf("Message privé envoyé à %s: %s\n", destinataire, contenu);
-        }  else if (strcmp(msg, "/man") == 0) {
+        } else if (strcmp(msg, "/man") == 0) {
             printf("Manuel :\n");
-            displayManual(); // Afficher le manuel
+            displayManual();
+        } else if (strcmp(msg, "/file") == 0) {
+            listFiles();
+            printf("Entrez le nom du fichier à envoyer : ");
+            fgets(msg, MAX_LEN, stdin);
+            msg[strcspn(msg, "\n")] = 0;
+            sendFileInfo(dS, msg);
+            sendFile(dS, msg);
+        } else {
+            printf("Message reçu: %s\n", msg);
         }
         pthread_mutex_unlock(&mutex);
-        if (nb_recv == -1)
-        {
+
+        if (nb_recv == -1) {
             perror("Erreur lors de la reception du message");
             pthread_mutex_lock(&mutex);
             is_connected = 0;
             pthread_mutex_unlock(&mutex);
             exit(EXIT_FAILURE);
-        }
-        else if (nb_recv == 0)
-        {
+        } else if (nb_recv == 0) {
             printf("Le serveur a fermé la connexion\n");
             pthread_mutex_lock(&mutex);
             is_connected = 0;
             pthread_mutex_unlock(&mutex);
             break;
         }
-        // Check if the message is "fin"
-        // If it is, close the socket and exit
-        if (strcmp(msg, "Le serveur est down.") == 0)
-        {
+
+        if (strcmp(msg, "Le serveur est down.") == 0) {
             pthread_mutex_lock(&mutex);
             is_connected = 0;
             pthread_mutex_unlock(&mutex);
             printf("Un client a interompu la communication\n");
             break;
-        }else
-        {
-            printf("Message reçu de %s: %s\n", pseudo, msg);
         }
     }
     close(dS);
     pthread_exit(0);
-   
 }
 
-void *writeMessage(void *arg)
-{
+void *writeMessage(void *arg) {
     int dS = *(int *)arg;
     int nb_send;
 
-    // Send the pseudo to the server
     nb_send = send(dS, pseudo, MAX_LEN, 0);
-    if (nb_send == -1)
-    {
+    if (nb_send == -1) {
         perror("Erreur lors de l'envoi du pseudo");
         pthread_mutex_lock(&mutex);
         is_connected = 0;
@@ -113,42 +147,34 @@ void *writeMessage(void *arg)
         pthread_exit(NULL);
     }
 
-    while (1)
-    {
+    while (1) {
         fgets(input, MAX_LEN, stdin);
         char *pos = strchr(input, '\n');
         *pos = '\0';
 
         pthread_mutex_lock(&mutex);
-        if (!is_connected)
-        {
+        if (!is_connected) {
             break;
         }
         pthread_mutex_unlock(&mutex);
-        
-        if (strcmp(input, "/man") == 0)
-        {
+
+        if (strcmp(input, "/man") == 0) {
             printf("Manuel : \n");
-            displayManual(); // Afficher le manuel
+            displayManual();
             printf("\n");
-            continue; // Ignorer l'envoi au serveur
+            continue;
         }
 
         printf("Message envoyé: %s\n", input);
 
-        // Send message
         nb_send = send(dS, input, MAX_LEN, 0);
-        if (nb_send == -1)
-        {
+        if (nb_send == -1) {
             perror("Erreur lors de l'envoi du message");
             pthread_mutex_lock(&mutex);
             is_connected = 0;
             pthread_mutex_unlock(&mutex);
             exit(EXIT_FAILURE);
-        }
-        else if (nb_send == 0)
-        {
-            // Connection closed by remote host
+        } else if (nb_send == 0) {
             printf("Le serveur a fermé la connexion\n");
             pthread_mutex_lock(&mutex);
             is_connected = 0;
@@ -156,10 +182,7 @@ void *writeMessage(void *arg)
             break;
         }
 
-        // Check if the message is "fin"
-        // If it is, close the socket and exit
-        if (strcmp(input, "/fin") == 0 || strcmp(input, "/kill") == 0)
-        {
+        if (strcmp(input, "/fin") == 0 || strcmp(input, "/kill") == 0) {
             printf("Vous avez mis fin à la discussion\n");
             pthread_mutex_lock(&mutex);
             is_connected = 0;
@@ -171,29 +194,23 @@ void *writeMessage(void *arg)
     pthread_exit(0);
 }
 
-void handle_sigint(int sig)
-{
+void handle_sigint(int sig) {
     printf("\nPour quitter, veuillez saisir le mot 'fin'.\n");
 }
 
-int main(int argc, char *argv[])
-{
-    if (argc != 4)
-    {
-        printf("Error: You must provide exactly 3 arguments.\n\
-                Usage: %s <server_ip> <server_port> <pseudo>\n",
-               argv[0]);
+int main(int argc, char *argv[]) {
+    if (argc != 4) {
+        printf("Error: You must provide exactly 3 arguments.\nUsage: %s <server_ip> <server_port> <pseudo>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
     printf("Début programme client\n");
     printf("Bienvenue sur la messagerie instantanée !\n");
 
-    strcpy(pseudo, argv[3]); // Capturer le pseudo
+    strcpy(pseudo, argv[3]);
 
     int dS = socket(PF_INET, SOCK_STREAM, 0);
-    if (dS == -1)
-    {
+    if (dS == -1) {
         perror("Erreur création socket");
         exit(EXIT_FAILURE);
     }
@@ -203,21 +220,17 @@ int main(int argc, char *argv[])
     struct sockaddr_in aS;
     aS.sin_family = AF_INET;
     int result = inet_pton(AF_INET, argv[1], &(aS.sin_addr));
-    if (result == 0)
-    {
+    if (result == 0) {
         fprintf(stderr, "Adresse invalide\n");
         exit(EXIT_FAILURE);
-    }
-    else if (result == -1)
-    {
+    } else if (result == -1) {
         perror("inet_pton");
         exit(EXIT_FAILURE);
     }
 
     aS.sin_port = htons(atoi(argv[2]));
     socklen_t lgA = sizeof(struct sockaddr_in);
-    if (connect(dS, (struct sockaddr *)&aS, lgA) == -1)
-    {
+    if (connect(dS, (struct sockaddr *)&aS, lgA) == -1) {
         perror("Erreur connect client");
         exit(EXIT_FAILURE);
     }
@@ -226,39 +239,34 @@ int main(int argc, char *argv[])
 
     signal(SIGINT, handle_sigint);
 
-    pthread_t readThread;
-    pthread_t writeThread;
+    pthread_t readThread, writeThread;
     pthread_mutex_init(&mutex, NULL);
 
-    if (pthread_create(&readThread, NULL, readMessage, &dS) != 0)
-    {
+    if (pthread_create(&readThread, NULL, readMessage, &dS) != 0) {
         perror("Erreur création thread de lecture");
         close(dS);
         exit(EXIT_FAILURE);
     }
 
-    if (pthread_create(&writeThread, NULL, writeMessage, &dS) != 0)
-    {
+    if (pthread_create(&writeThread, NULL, writeMessage, &dS) != 0) {
         perror("Erreur création thread d'écriture");
         close(dS);
         exit(EXIT_FAILURE);
     }
 
-    if (pthread_join(readThread, NULL) != 0)
-    {
+    if (pthread_join(readThread, NULL) != 0) {
         perror("Erreur fermeture thread de lecture");
         close(dS);
         exit(EXIT_FAILURE);
     }
 
-    if (pthread_join(writeThread, NULL) != 0)
-    {
+    if (pthread_join(writeThread, NULL) != 0) {
         perror("Erreur fermeture thread d'écriture");
         close(dS);
         exit(EXIT_FAILURE);
     }
-    close(dS);
 
+    close(dS);
     pthread_mutex_destroy(&mutex);
 
     return EXIT_SUCCESS;
--- serveurv2.1.c
+++ serveurv2.1.c
@@ -11,174 +11,115 @@
 #define MAX_CLIENT 20
 #define BUFFER_SIZE 1000
 
-struct Client
-{
+struct Client {
     int socket;
     char pseudo[BUFFER_SIZE];
 };
 
-struct Message
-{
+struct Message {
     char pseudo[BUFFER_SIZE];
     char contenu[BUFFER_SIZE];
 };
 
 pthread_t tab_thread[MAX_CLIENT];
 struct Client tab_client[MAX_CLIENT];
-
 struct sockaddr_in tab_adr[MAX_CLIENT];
 socklen_t tab_lg[MAX_CLIENT];
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
-pthread_mutex_t mutex =
-    PTHREAD_MUTEX_INITIALIZER; // Mutex pour protéger les variables partagées
+bool is_pseudo_taken(const char *pseudo) {
+    for (int i = 0; i < MAX_CLIENT; i++) {
+        if (tab_client[i].socket != -1 && strcmp(tab_client[i].pseudo, pseudo) == 0) {
+            return true;
+        }
+    }
+    return false;
+}
 
-// Fonction exécutée par chaque thread pour relayer les messages
-void *broadcast(void *client)
-{
+void *broadcast(void *client) {
     struct Client *data = (struct Client *)client;
     int dSC = data->socket;
-    char pseudo[BUFFER_SIZE];
-    strcpy(pseudo, data->pseudo); // Copiez le pseudo du client
     struct Message message;
-    int nb_recv;
-    int nb_send;
+    int nb_recv, nb_send;
     int i;
 
-    // Boucle infinie pour écouter les messages du client
-    while (1)
-    {
-        // Recevoir le message du client
+    while (1) {
         nb_recv = recv(dSC, &message.contenu, BUFFER_SIZE, 0);
-        if (nb_recv == -1)
-        {
+        if (nb_recv == -1) {
             perror("Erreur lors de la reception");
-            exit(EXIT_FAILURE);
-        }
-        else if (nb_recv == 0)
-        {
+            break;
+        } else if (nb_recv == 0) {
             printf("Client déconnecté\n");
             break;
         }
-        else if (strcmp(message.contenu, "/kill") == 0) {
+
+        if (strcmp(message.contenu, "/kill") == 0) {
             char messFin[BUFFER_SIZE] = "Le serveur est down.";
-            for (i = 0; i < MAX_CLIENT; i++)
-            {
-                if (tab_client[i].socket != -1 )
-                {
-                    
+            for (i = 0; i < MAX_CLIENT; i++) {
+                if (tab_client[i].socket != -1) {
                     nb_send = send(tab_client[i].socket, &messFin, BUFFER_SIZE, 0);
-                    
-                    
-                    if (nb_send == -1)
-                    {
+                    if (nb_send == -1) {
                         perror("Erreur lors de l'envoi");
-                        exit(EXIT_FAILURE);
                     }
-                    if (nb_send == 0)
-                    {
-                        printf("%s s'est déconnecté\n", tab_client[i].pseudo);
-                        pthread_mutex_lock(&mutex);
-                        tab_client[i].socket = -1;
-                        pthread_mutex_unlock(&mutex);
-                    }
-                    
                 }
             }
             break;
-        }
-        else if (strcmp(message.contenu, "/fin") == 0)
-        { // Si le client envoie "fin", arrêter la discussion pour ce client
+        } else if (strcmp(message.contenu, "/fin") == 0) {
             printf("Fin de la discussion pour client : %s\n", data->pseudo);
             break;
-        }
-        else if (strncmp(message.contenu, "/mp", 3) == 0)
-        { // Si le client envoie la commande /mp pour envoyer un message privé
+        } else if (strncmp(message.contenu, "/mp", 3) == 0) {
             char destinataire[BUFFER_SIZE];
             char contenu[BUFFER_SIZE];
             sscanf(message.contenu, "/mp %s %[^\n]", destinataire, contenu);
             bool destinataire_trouve = false;
-            for (i = 0; i < MAX_CLIENT; i++)
-            {
-                if (strcmp(tab_client[i].pseudo, destinataire) == 0)
-                {
-                    nb_send = send(tab_client[i].socket, contenu, strlen(contenu), 0);
-                    if (nb_send == -1)
-                    {
+            for (i = 0; i < MAX_CLIENT; i++) {
+                if (strcmp(tab_client[i].pseudo, destinataire) == 0) {
+                    nb_send = send(tab_client[i].socket, contenu, BUFFER_SIZE, 0);
+                    if (nb_send == -1) {
                         perror("Erreur lors de l'envoi");
-                        exit(EXIT_FAILURE);
                     }
                     destinataire_trouve = true;
                     break;
                 }
             }
-            if (!destinataire_trouve)
-            {
+            if (!destinataire_trouve) {
                 char erreur_message[BUFFER_SIZE];
                 snprintf(erreur_message, BUFFER_SIZE, "Le destinataire '%s' n'existe pas.", destinataire);
                 nb_send = send(dSC, erreur_message, strlen(erreur_message), 0);
-                if (nb_send == -1)
-                {
+                if (nb_send == -1) {
                     perror("Erreur lors de l'envoi");
-                    exit(EXIT_FAILURE);
                 }
             }
-        }
-        else
-        { // Envoyer le message aux autres clients
+        } else {
             printf("%s : %s\n", data->pseudo, message.contenu);
-
-            for (i = 0; i < MAX_CLIENT; i++)
-            {
-                if (tab_client[i].socket != -1 && tab_client[i].socket != dSC)
-                {
+            for (i = 0; i < MAX_CLIENT; i++) {
+                if (tab_client[i].socket != -1 && tab_client[i].socket != dSC) {
                     nb_send = send(tab_client[i].socket, &message.contenu, BUFFER_SIZE, 0);
-                    if (nb_send == -1)
-                    {
+                    if (nb_send == -1) {
                         perror("Erreur lors de l'envoi");
-                        exit(EXIT_FAILURE);
-                    }
-                    if (nb_send == 0)
-                    {
-                        printf("%s s'est déconnecté\n", tab_client[i].pseudo);
-                        pthread_mutex_lock(&mutex);
-                        tab_client[i].socket = -1;
-                        pthread_mutex_unlock(&mutex);
                     }
                 }
             }
         }
     }
+
     if (strcmp(message.contenu, "/kill") == 0) {
-        
-        for (i = 0; i < MAX_CLIENT; i++){
-
-            if (tab_client[i].socket != -1 && tab_client[i].socket != dSC)
-            {
-                // Fermer le socket du client
-                if (close(tab_client[i].socket) == -1)
-                {
+        for (i = 0; i < MAX_CLIENT; i++) {
+            if (tab_client[i].socket != -1 && tab_client[i].socket != dSC) {
+                if (close(tab_client[i].socket) == -1) {
                     perror("Erreur lors de la fermeture du descripteur de fichier");
-                    exit(EXIT_FAILURE);
                 }
-
-            // Mettre -1 dans le tableau pour indiquer que le client s'est déconnecté
                 pthread_mutex_lock(&mutex);
                 tab_client[i].socket = -1;
                 pthread_mutex_unlock(&mutex);
             }
         }
-
     }
-    
-    // Fermer le socket du client
-    if (close(dSC) == -1)
-    {
+
+    if (close(dSC) == -1) {
         perror("Erreur lors de la fermeture du descripteur de fichier");
-        exit(EXIT_FAILURE);
     }
-    
 
-    // Mettre -1 dans le tableau pour indiquer que le client s'est déconnecté
     pthread_mutex_lock(&mutex);
     data->socket = -1;
     pthread_mutex_unlock(&mutex);
@@ -186,58 +127,69 @@ void *broadcast(void *client)
     pthread_exit(0);
 }
 
-// fonction qui permet à un client de se connecter et de mettre à jour le
-// tab_client
-void *connexion(void *dServeur)
-{
+void *connexion(void *dServeur) {
     int dS = *(int *)dServeur;
-    struct Message message;
-    while (1)
-    {
+    char pseudo[BUFFER_SIZE];
 
-        // Accepter les connexions des clients
-        for (int i = 0; i < MAX_CLIENT; i++)
-        {
-            if (tab_client[i].socket == -1)
-            { // on accepte les nouveaux clients dans des cases non occupés
+    while (1) {
+        for (int i = 0; i < MAX_CLIENT; i++) {
+            if (tab_client[i].socket == -1) {
                 tab_lg[i] = sizeof(struct sockaddr_in);
-                pthread_mutex_lock(&mutex);
-                tab_client[i].socket = accept(dS, (struct sockaddr *)&tab_adr[i], &tab_lg[i]);
-                pthread_mutex_unlock(&mutex);
-                if (tab_client[i].socket == -1)
-                {
+                int client_socket = accept(dS, (struct sockaddr *)&tab_adr[i], &tab_lg[i]);
+                if (client_socket == -1) {
                     perror("Erreur lors de la connexion avec le client");
-                    exit(EXIT_FAILURE);
+                    continue;
                 }
-                recv(tab_client[i].socket, tab_client[i].pseudo, sizeof(tab_client[i].pseudo), 0);
+
+                // Recevoir le pseudo du client
+                int recv_ret = recv(client_socket, pseudo, sizeof(pseudo), 0);
+                if (recv_ret <= 0) {
+                    perror("Erreur lors de la reception du pseudo");
+                    close(client_socket);
+                    continue;
+                }
+
+                pthread_mutex_lock(&mutex);
+                if (is_pseudo_taken(pseudo)) {
+                    char error_message[] = "Pseudo déjà utilisé. Connexion refusée.";
+                    send(client_socket, error_message, sizeof(error_message), 0);
+                    close(client_socket);
+                    pthread_mutex_unlock(&mutex);
+                    continue;
+                } else {
+                    strcpy(tab_client[i].pseudo, pseudo);
+                    tab_client[i].socket = client_socket;
+                    pthread_mutex_unlock(&mutex);
+                }
+
                 printf("%s s'est connecté\n", tab_client[i].pseudo);
-                if (pthread_create(&tab_thread[i], NULL, broadcast, (void *)&tab_client[i]) != 0)
-                {
+
+                if (pthread_create(&tab_thread[i], NULL, broadcast, (void *)&tab_client[i]) != 0) {
                     perror("Erreur lors de la création du thread");
-                    exit(EXIT_FAILURE);
+                    close(client_socket);
+                    pthread_mutex_lock(&mutex);
+                    tab_client[i].socket = -1;
+                    pthread_mutex_unlock(&mutex);
+                    continue;
                 }
             }
         }
-        // Attendre que les threads se terminent
-        for (int i = 0; i < MAX_CLIENT; i++)
-        {
-            if (tab_client[i].socket != -1)
-            {
-                if (pthread_join(tab_thread[i], NULL) != 0)
-                {
+
+        for (int i = 0; i < MAX_CLIENT; i++) {
+            if (tab_client[i].socket != -1) {
+                if (pthread_join(tab_thread[i], NULL) != 0) {
                     perror("Erreur lors de l'attente de la fin du thread");
                     exit(EXIT_FAILURE);
                 }
             }
         }
     }
+
     pthread_exit(0);
 }
 
-int main(int argc, char *argv[])
-{
-    if (argc != 2)
-    {
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
         printf("Error: You must provide exactly 1 argument.\nUsage: ./serv <port>\n");
         exit(EXIT_FAILURE);
     }
@@ -245,8 +197,7 @@ int main(int argc, char *argv[])
     printf("Debut du Serveur.\n");
 
     int dS = socket(PF_INET, SOCK_STREAM, 0);
-    if (dS == -1)
-    {
+    if (dS == -1) {
         perror("Erreur lors de la création du socket");
         exit(EXIT_FAILURE);
     }
@@ -255,45 +206,43 @@ int main(int argc, char *argv[])
     ad.sin_family = AF_INET;
     ad.sin_addr.s_addr = INADDR_ANY;
     ad.sin_port = htons(atoi(argv[1]));
-    if (bind(dS, (struct sockaddr *)&ad, sizeof(ad)) == -1)
-    {
+
+    if (bind(dS, (struct sockaddr *)&ad, sizeof(ad)) == -1) {
         perror("Erreur lors du nommage du socket");
         exit(EXIT_FAILURE);
     }
 
-    if (listen(dS, 10) == -1)
-    {
+    if (listen(dS, 10) == -1) {
         perror("Erreur lors du passage en mode écoute");
         exit(EXIT_FAILURE);
     }
-    for (int i = 0; i < MAX_CLIENT; i++)
-    {
+
+    for (int i = 0; i < MAX_CLIENT; i++) {
         tab_client[i].socket = -1;
     }
+
     pthread_mutex_init(&mutex, NULL);
-    
+
     pthread_t relai;
-    if (pthread_create(&relai, NULL, connexion, (void *)&dS) != 0)
-    {
+    if (pthread_create(&relai, NULL, connexion, (void *)&dS) != 0) {
         perror("Erreur lors de la création du thread");
         exit(EXIT_FAILURE);
     }
+
     printf("En attente de connexion... \n");
 
-    if (pthread_join(relai, NULL) != 0)
-    {
+    if (pthread_join(relai, NULL) != 0) {
         perror("Erreur lors de l'attente de la fin du thread");
         exit(EXIT_FAILURE);
     }
+
     pthread_mutex_destroy(&mutex);
-    
     printf("Les clients sont déconnectés\n");
 
-    if (close(dS) == -1)
-    {
+    if (close(dS) == -1) {
         perror("Erreur lors de la fermeture du socket de acceptation");
         exit(EXIT_FAILURE);
     }
 
-    return 1;
+    return 0;
 }
